ea60186fe8d5b9f399edb13c15bfac4b83304025
Prem Chowdhry <prem@dyn3146-73.wlan.ic.ac.uk>
2018-05-29 15:41:30 +0100

    Added write to file in assembler

---
+++ cmake-build-debug/CMakeFiles/arm11_34.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/arm11_34.dir/src/assemble.c.o"
+  "CMakeFiles/arm11_34.dir/src/emulate.c.o"
+  "arm11_34.pdb"
+  "arm11_34"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/arm11_34.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/assemble.c
+++ src/assemble.c
@@ -1,5 +1,47 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+
+void printBits(uint32_t x) {
+    int i;
+    uint32_t mask = 1 << 31;
+    for (i=0; i<32; ++i) {
+        if ((x & mask) == 0) {
+            printf("0");
+        }
+        else
+        {
+            printf("1");
+        }
+        x = x << 1;
+    }
+    printf("\n");
+}
 
 int main(int argc, char **argv) {
   return EXIT_SUCCESS;
+}
+
+void writeToFile(const char* sourceName, const char* destinationName) {
+    FILE* sourceFile = fopen(sourceName, "rb");
+    if (sourceFile == NULL) {
+        perror("Error opening the source binary file!");
+        exit(EXIT_FAILURE);
+    }
+
+    FILE* destFile = fopen(destinationName, "wb");
+    if (destFile == NULL) {
+        perror("Error opening the destination binary file!");
+        exit(EXIT_FAILURE);
+    }
+
+    char c = fgetc(sourceFile);
+    while (c != EOF) {
+        fputc(c, destFile);
+        fgetc(sourceFile);
+    }
+
+    fclose(sourceFile);
+    fclose(destFile);
 }
