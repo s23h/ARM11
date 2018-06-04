f61ad91ff537dc75c84219b80dedf78bce33cd1e
Prem Chowdhry <prem@Prems-MacBook-Pro.local>
2018-06-04 01:24:09 +0100

    Done tokeniser, need to change tthe malloc

--- src/assemble.c
+++ src/assemble.c
@@ -23,14 +23,14 @@ int main(int argc, char **argv) {
   return EXIT_SUCCESS;
 }
 
-void writeToFile(const char* sourceName, const char* destinationName) {
+void writeToFile(const char* sourceName, const char* destName) {
     FILE* sourceFile = fopen(sourceName, "rb");
     if (sourceFile == NULL) {
         perror("Error opening the source binary file!");
         exit(EXIT_FAILURE);
     }
 
-    FILE* destFile = fopen(destinationName, "wb");
+    FILE* destFile = fopen(destName, "wb");
     if (destFile == NULL) {
         perror("Error opening the destination binary file!");
         exit(EXIT_FAILURE);
@@ -39,9 +39,46 @@ void writeToFile(const char* sourceName, const char* destinationName) {
     char c = fgetc(sourceFile);
     while (c != EOF) {
         fputc(c, destFile);
-        fgetc(sourceFile);
+        c = fgetc(sourceFile);
     }
 
     fclose(sourceFile);
     fclose(destFile);
+}
+
+char*** tokenise(const char* fileName)
+{
+    FILE* file = fopen(fileName, "r"); /* should check the result */
+    char line[511];
+
+    while (fgets(line, sizeof(line), file)) {
+        char** token = breakDown(line, ' ');
+    }
+    /* may check feof here to make a difference between eof and io failure -- network
+       timeout for instance */
+
+    fclose(file);
+}
+
+char** breakDown(char *line, const char *delim) {
+    char **array = malloc(sizeof(char *));
+    if (array) {
+        size_t n = 1;
+        char *token = strtok(line, delim);
+        while (token) {
+            char **tmp = realloc(array, (n + 1) * sizeof(char *));
+            if (tmp == NULL) {
+                break;
+            }
+            array = tmp;
+            ++n;
+            array[n - 2] = malloc(strlen(token) + 1);
+            if (array[n - 2] != NULL) {
+                strcpy(array[n - 2], token);
+            }
+            token = strtok(NULL, delim);
+        }
+        array[n - 1] = NULL;
+    }
+    return array;
 }
