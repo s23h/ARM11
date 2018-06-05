d3f6f956a6703fab3102d19684e9775d58c36b70
Salman Hussain <salman@dyn3203-224.wlan.ic.ac.uk>
2018-06-05 17:34:56 +0100

    Assembles data processing - untested

--- src/assemble.c
+++ src/assemble.c
@@ -92,3 +92,143 @@ char** breakDown(char *line, const char *delim) {
     }
     return array;
 }
+
+typedef enum { DATA_PROCESSING, MULTIPLY, DATA_TRANSFER, BRANCH } instructionType;
+
+typedef struct {
+    instructionType type;
+    uint8_t cond;
+    uint8_t i;
+    uint8_t opcode;
+    uint8_t s;
+    uint8_t rn;
+    uint8_t rd;
+    uint16_t operand2;
+    uint8_t a;
+    uint8_t rs;
+    uint8_t rm;
+    uint8_t p;
+    uint8_t u;
+    uint8_t l;
+    uint8_t rest;
+    uint32_t offset;
+} decodedInstruction;
+
+static bool is_label(char *instruction) {
+  return instruction[strlen(instruction) - 1] == ':';
+}
+
+void remove_colon(char *str) {
+  //replaces ':' with EOF
+  str[length - 1] = '\0';
+}
+
+void parseArgumentsDP(decodedInstruction d, char** arguments) {
+  d.s = 0;
+  d.rd = (int) strtol(tokens[i][1]++, (char **)NULL, 10);
+  d.rn = (int) strtol(tokens[i][2]++, (char **)NULL, 10);
+  if (tokens[i][3][0] == '#'){
+    d.i = 1;
+    int base = (tokens[i][3][1] == '0' && tokens[i][3][1] == 'x') ? 16 : 10;
+    d.operand2 = (int) strtol(tokens[i][1]++, (char **)NULL, base);
+  } else {
+    d.i = 0;
+    d.operand2 = (int) strtol(tokens[i][3]++, (char **)NULL, 10);
+  }
+}
+
+decodedInstruction* readTokens(char*** tokens) {
+  decodedInstruction* instructions = malloc(sizeof(tokens)*sizeof(decodedInstruction));
+
+  for (int i=0; i<sizeof(tokens); i++) {
+      if (is_label(tokens[i][0])) {
+        remove_colon(tokens[i][0]);
+      } else {
+        decodedInstruction d;
+
+        if (!strcmp(tokens[i][0], "and")) {
+          d.type = DATA_PROCESSING;
+          d.opcode = 0;
+          d.cond = 14;
+          parseArgumentsDP(d, tokens[i]);
+          instructions[i] = d;
+        }
+        if (!strcmp(tokens[i][0], "eor")) {
+          d.type = DATA_PROCESSING;
+          d.opcode = 1;
+          d.cond = 14;
+          parseArgumentsDP(d, tokens[i]);
+        }
+        if (!strcmp(tokens[i][0], "sub")) {
+          d.type = DATA_PROCESSING;
+          d.opcode = 2;
+          d.cond = 14;
+          parseArgumentsDP(d, tokens[i]);
+        }
+        if (!strcmp(tokens[i][0], "rsb")) {
+          d.type = DATA_PROCESSING;
+          d.opcode = 3;
+          d.cond = 14;
+          parseArgumentsDP(d, tokens[i]);
+        }
+        if (!strcmp(tokens[i][0], "add")) {
+          d.type = DATA_PROCESSING;
+          d.opcode = 4;
+          d.cond = 14;
+          parseArgumentsDP(d, tokens[i]);
+        }
+        if (!strcmp(tokens[i][0], "oor")) {
+          d.type = DATA_PROCESSING;
+          d.opcode = 12;
+          d.cond = 14;
+          parseArgumentsDP(d, tokens[i]);
+        }
+        if (!strcmp(tokens[i][0], "mov")) {
+          d.type = DATA_PROCESSING;
+          d.opcode = 13;
+          d.cond = 14;
+          d.s = 0;
+          d.rd = (int) strtol(tokens[i][1]++, (char **)NULL, 10);
+          if (tokens[i][2][0] == '#'){
+            d.i = 1;
+            int base = (tokens[i][3][1] == '0' && tokens[i][3][1] == 'x') ? 16 : 10;
+            d.operand2 = (int) strtol(tokens[i][1]++, (char **)NULL, base);
+          } else {
+            d.i = 0;
+            d.operand2 = (int) strtol(tokens[i][3]++, (char **)NULL, 10);
+          }
+        }
+
+        if (!strcmp(tokens[i][0], "tst")) {
+          d.type = DATA_PROCESSING;
+          d.opcode = 8;
+          d.cond = 14;
+          parseArgumentsDP(d, tokens[i]);
+          d.s = 1;
+        }
+
+        if (!strcmp(tokens[i][0], "teq")) {
+          d.type = DATA_PROCESSING;
+          d.opcode = 9;
+          d.cond = 14;
+          parseArgumentsDP(d, tokens[i]);
+          d.s = 1;
+        }
+
+        if (!strcmp(tokens[i][0], "cmp")) {
+          d.type = DATA_PROCESSING;
+          d.opcode = 10;
+          d.cond = 14;
+          parseArgumentsDP(d, tokens[i]);
+          d.s = 1;
+        }
+
+        instructions[i] = d;
+
+      }
+  }
+
+
+
+
+}
