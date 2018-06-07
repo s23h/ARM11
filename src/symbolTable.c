cd9ffc7223549c62c8039eb167a01472422a6b35
Zimu Zeng <zimu.zeng17@ic.ac.uk>
2018-06-07 17:52:36 +0100

    Symbol table ADT implemented, tested and working.

---
+++ src/symbolTable.c
@@ -0,0 +1,93 @@
+#include "symbolTable.h"
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+// This file defines a symbol table ADT, based on the implementation of the doubly linked list from the C lectures.
+
+// Allocates memory for a key-value pair
+pair* allocatePair(void) {
+    pair* newPair = malloc(sizeof(pair));
+    if (newPair == NULL) {
+        perror("allocatePair");
+        exit(EXIT_FAILURE);
+    }
+
+    return newPair;
+};
+
+// Frees the memory that holds the specified pair
+void freePair(pair* pairToFree) {
+    free(pairToFree);
+}
+
+// Initialises a new symbol table
+void initTable(symbolTable* t) {
+    t -> header = allocatePair();
+    t -> footer = allocatePair();
+    t -> header -> prev = NULL;
+    t -> footer -> next = NULL;
+    t -> header -> next = t -> footer;
+    t -> footer -> prev = t -> header;
+}
+
+// Returns a iterator pointing to the beginning of the list.
+tableIter tableBegin(symbolTable* t) {
+    return t -> header -> next;
+}
+
+// Returns a iterator pointing to one past the end of the list.
+tableIter tableEnd(symbolTable *t) {
+    return t -> footer;
+}
+
+// Inserts a new pair before the current iterator.
+void tableInsert(symbolTable* t, tableIter iter, char* key, uint32_t value) {
+    pair* newPair = allocatePair();
+    newPair -> value = value;
+    newPair -> key = key;
+
+    newPair -> prev = iter -> prev;
+    newPair -> next = iter;
+
+    iter -> prev -> next = newPair;
+    iter -> prev = newPair;
+}
+
+// Inserts a new pair at the front of the list.
+void insertFront(symbolTable* t, char* key, uint32_t value) {
+    tableInsert(t, tableBegin(t), key, value);
+}
+
+// Inserts a new pair at the back of the list.
+void insertBack(symbolTable* t, char* key, uint32_t value) {
+    tableInsert(t, tableEnd(t), key, value);
+}
+
+void destroyTable(symbolTable* t) {
+    pair* p = t -> header;
+    while (p != NULL) {
+        pair* next = p -> next;
+        freePair(p);
+        p = next;
+    }
+}
+
+// Looks up the value for the given symbol, assumes that all keys being looked up are present in the table.
+uint32_t tableLookup(symbolTable* t, char* key) {
+    for (tableIter iter = tableBegin(t); iter != tableEnd(t); iter = iter -> next) {
+        if (strcmp(iter -> key, key) == 0) {
+            return iter -> value;
+        }
+    }
+    // Should be unreachable.
+    return 0;
+}
+
+
+
+
+
+
+
---
+++ src/symbolTable.h
@@ -0,0 +1,58 @@
+#ifndef SYMBOL_TABLE_H
+#define SYMBOL_TABLE_H
+
+#include <stdint.h>
+
+// This file defines a symbol table ADT, based on the implementation of the doubly linked list from the C lectures.
+
+// Represents a string-int pair in the symbol table
+struct keyValuePair {
+    char* key;
+    uint32_t value;
+    struct keyValuePair* next;
+    struct keyValuePair* prev;
+};
+
+typedef struct keyValuePair pair;
+
+typedef struct {
+    pair *header;
+    pair *footer;
+} symbolTable;
+
+// Allocates memory for a key-value pair
+pair* allocatePair(void);
+
+// Frees the memory that holds the specified pair
+void freePair(pair* pairToFree);
+
+// Initialises a new symbol table
+void initTable(symbolTable* t);
+
+typedef pair* tableIter;
+
+// Returns a iterator pointing to the beginning of the list.
+tableIter tableBegin(symbolTable* t);
+
+// Returns a iterator pointing to one past the end of the list.
+tableIter tableEnd(symbolTable *t);
+
+// Inserts a new pair before the current iterator.
+void tableInsert(symbolTable* t, tableIter iter, char* key, uint32_t value);
+
+// Inserts a new pair at the front of the list.
+void insertFront(symbolTable* t, char* key, uint32_t value);
+
+// Inserts a new pair at the back of the list.
+void insertBack(symbolTable* t, char* key, uint32_t value);
+
+void destroyTable(symbolTable* t);
+
+uint32_t tableLookup(symbolTable* t, char* key);
+
+#endif
+
+
+
+
+
